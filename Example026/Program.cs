6c0544d7de686860bff83687d8efa00e8b57034a
Evgeniy <zadrod93@yandex.ru>
2022-02-13 22:59:10 +0300

    Задача номер 26

---
+++ Example026/Program.cs
@@ -0,0 +1,18 @@
+﻿//26 Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine());
+
+int count = 1;
+int result = 1;
+
+while (count <= numB)
+{
+    result = result * numA;
+    count++;
+}
+
+Console.WriteLine("Первое число в степени второго = " + result);
